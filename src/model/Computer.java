f338a66e6cc7977800043dbb894b94c598988eaa
AndresTea17 <pipeth1701@gmail.com>
2025-10-24 12:58:36 -0500

    hola

--- src/model/Computer.java
+++ src/model/Computer.java
@@ -1,25 +1,24 @@
 package model;
+import java.util.ArrayList;
 
 public class Computer {
-
+    private String serialNumber;
+    private boolean NextWindow;
+    private ArrayList<Incident> incidents;
     /*
      * ATENCION !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
      * Agregue los atributos (relaciones) necesarios para satisfacer los
      * requerimientos.
      */
 
-    public Computer() {
-
+    public Computer(String Numero, boolean Window) {
+        this.serialNumber = Numero;
+        this.NextWindow = Window;
+        this.incidents = new ArrayList<>();
     }
 
-    /*
-     * ATENCION !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-     * El siguiente metodo esta incompleto.
-     * Agregue los parametros y retorno que sean pertinentes.
-     * Agregue la logica necesaria.
-     */
-    public void addIncident() {
-
+    public void addIncident(LocalDate Hora, String Descripcion) {
+        incidents.add(new Incident())
     }
 
 }
--- src/model/Incident.java
+++ src/model/Incident.java
@@ -1,5 +1,17 @@
 package model;
+import java.time.LocalDate;
 
 public class Incident {
+    private String Description;
+    private boolean Solution;
+    private int SolutionHours;
+    private LocalDate DateReport;
+
+    public Incident(LocalDate Hora, String Descripcion) {
+        this.DateReport = Hora;
+        this.Description = Descripcion;
+    }
+
+
     
 }
